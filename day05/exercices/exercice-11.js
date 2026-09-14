1afcc70f99c7f38a2ee244865a528f9701047f54
fatima zahra tajjouni <fatimazahraetajjouni3@gmail.com>
2026-09-14 11:34:18 +0100

    jour 05 : terminer les exercices 01 a 10

---
+++ day05/exercices/exercice-11.js
@@ -0,0 +1,22 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 05 · EXERCICE 11 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * GÉNÉRATEUR D'ACRONYME
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * À partir de la phrase "Hyper Text Markup Language", retournez "HTML".
+ * Indice : Découpez en mots, bouclez, prenez la première lettre de chaque mot, mettez en majuscule, et rejoignez le tout.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-11
+ * ▶️ Commande : node day05/exercices/exercice-11.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+phrase = "Hyper Text Markup Language";
+let mots = phrase.split();
+let premieres_lettres = [mot[0].upper() for() mot in mots];
+let sigle = "".join(premieres_lettres);
+print(sigle);
